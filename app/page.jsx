7710c11209f13579eed16fb6f3e1a30f39db2bac
HuongNTN <huongnguyen08.cv@gmail.com>
2025-09-20 23:22:02 +0700

    update home

--- app/page.jsx
+++ app/page.jsx
@@ -91,14 +91,14 @@ export default function HomePage(){
                   </div>
                   <h6>Book Appointment</h6>
                 </a>
-                <a href="/" className="list-item">
+                <a href="/gallery" className="list-item">
                   <div className="list-icon bg-pink">
                     <img src="/theme/assets/img/icons/list-icon-03.svg" alt="img" />
                   </div>
-                  <h6>Services</h6>
+                  <h6>galleries</h6>
                 </a>
 
-                <a href="/photographer" className="list-item">
+                <a href="/search" className="list-item">
                   <div className="list-icon bg-primary">
                     <img src="/theme/assets/img/icons/list-icon-02.svg" alt="img" />
                   </div>
@@ -115,6 +115,152 @@ export default function HomePage(){
           </div>
         </div>
       </div>
+      <section class="reason-section">
+				<div class="container">
+					<div class="section-header sec-header-one text-center aos" data-aos="fade-up">
+						<span class="badge badge-primary">Why Book With Us</span>
+						<h2>Compelling Reasons to Choose</h2>
+					</div>
+					<div class="row row-gap-4 justify-content-center">
+						<div class="col-lg-4 col-md-6">
+							<div class="reason-item aos" data-aos="fade-up">
+								<h6 class="mb-2 d-flex align-items-center"><i class="isax isax-tag-user5 text-orange me-2"></i>Follow-Up Care</h6>
+								<p class="fs-14 mb-0">We ensure continuity of care through regular follow-ups and communication, helping you stay on track with health goals.</p>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-6">
+							<div class="reason-item aos" data-aos="fade-up">
+								<h6 class="mb-2 d-flex align-items-center"><i class="isax isax-voice-cricle text-purple me-2"></i>Patient-Centered Approach</h6>
+								<p class="fs-14 mb-0">We prioritize your comfort and preferences, tailoring our services to meet your individual needs and Care from Our Experts</p>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-6">
+							<div class="reason-item aos" data-aos="fade-up">
+								<h6 class="mb-2 d-flex align-items-center"><i class="isax isax-wallet-add-15 text-cyan me-2"></i>Convenient Access</h6>
+								<p class="fs-14 mb-0">Easily book appointments online or through our dedicated customer service team, with flexible hours to fit your schedule.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+     <section className="bookus-section bg-dark">
+				<div className="container">
+					<div className="row align-items-center row-gap-4">
+						<div className="col-lg-6">
+							<div className="bookus-img">
+								<div className="row g-3">
+									<div className="col-md-12 aos" data-aos="fade-up">
+										<img src="theme/assets/img/book-01.jpg" alt="img" style={{width: "100%"}} className="img-fluid"/>
+									</div>
+									<div className="col-sm-6 aos" data-aos="fade-up">
+										<img src="theme/assets/img/book-02.jpg" alt="img" className="img-fluid"/>
+									</div>
+									<div className="col-sm-6 aos" data-aos="fade-up">
+										<img src="theme/assets/img/book-03.jpg" alt="img" className="img-fluid"/>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className="col-lg-6">
+							<div className="section-header sec-header-one mb-2 aos" data-aos="fade-up">
+								<span className="badge badge-primary">Why Book With Us</span>
+								<h2 className="text-white mb-3">We are committed to understanding your <span className="text-primary-gradient"> unique vision and capturing it with care.</span></h2>
+							</div>
+							<p className="text-light mb-4">As a trusted photography partner in our community, we are passionate about celebrating life’s moments beyond the studio. We actively engage in community events, creative exhibitions, and educational workshops.</p>
+							<div className="faq-info aos" data-aos="fade-up">
+								<div className="accordion" id="faq-details">
+
+									<div className="accordion-item">
+										<h2 className="accordion-header" id="headingOne">
+											<a href="javascript:void(0);" className="accordion-button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+												01 . Our Vision
+											</a>
+										</h2>
+										<div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#faq-details">
+											<div className="accordion-body">
+												<div className="accordion-content">
+													<p>We envision a community where everyone has access to high-quality photography and the inspiration they need to capture and cherish life’s most meaningful moments</p>
+												</div> 
+											</div>
+										</div>
+									</div>
+									<div className="accordion-item">
+										<h2 className="accordion-header" id="headingTwo">
+											<a href="javascript:void(0);" className="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-controls="collapseTwo">
+												02 . Our Mission
+											</a>
+										</h2>
+										<div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#faq-details">
+											<div className="accordion-body">
+												<div className="accordion-content">
+													<p>Our mission is to make high-quality photography accessible to everyone, empowering people to capture, share, and treasure life’s most meaningful moments.</p>
+												</div> 
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="bookus-sec">
+						<div className="row g-4">
+							<div className="col-lg-3">
+								<div className="book-item">
+									<div className="book-icon bg-primary">
+										<i className="isax isax-search-normal5"></i>
+									</div>
+									<div className="book-info">
+										<h6 className="text-white mb-2">Search For Photographers</h6>
+										<p className="fs-14 text-light">Easily search and connect with professional photographers who match your unique style and needs.</p>
+									</div>
+									<div className="way-icon">
+										<img src="theme/assets/img/icons/way-icon.svg" alt="img"/>
+									</div>
+								</div>
+							</div>
+							<div className="col-lg-3">
+								<div className="book-item">
+									<div className="book-icon bg-orange">
+										<i className="isax isax-security-user5"></i>
+									</div>
+									<div className="book-info">
+										<h6 className="text-white mb-2">Check Photographer Profile</h6>
+										<p className="fs-14 text-light">View detailed photographer profiles to explore their portfolio, style, and experience.</p>
+									</div>
+									<div className="way-icon">
+										<img src="theme/assets/img/icons/way-icon.svg" alt="img"/>
+									</div>
+								</div>
+							</div>
+							<div className="col-lg-3">
+								<div className="book-item">
+									<div className="book-icon bg-cyan">
+										<i className="isax isax-calendar5"></i>
+									</div>
+									<div className="book-info">
+										<h6 className="text-white mb-2">Schedule Appointment</h6>
+										<p className="fs-14 text-light">After choose your preferred photographer, select a convenient time slot, & confirm your appointment.</p>
+									</div>
+									<div className="way-icon">
+										<img src="theme/assets/img/icons/way-icon.svg" alt="img"/>
+									</div>
+								</div>
+							</div>
+							<div className="col-lg-3">
+								<div className="book-item">
+									<div className="book-icon bg-indigo">
+										<i className="isax isax-blend5"></i>
+									</div>
+									<div className="book-info">
+										<h6 className="text-white mb-2">Get Your Solution</h6>
+										<p className="fs-14 text-light">Receive tailored photography solutions designed to match your unique needs and style.</p>
+									</div>
+								</div>
+							</div>
+						</div>		
+					</div>		
+				</div>
+			</section>
     </>
   )
 }
